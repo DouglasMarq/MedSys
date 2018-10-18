7db80aa35985846e9adef93fcd83ad81abcfcbaf
Douglas Marques <douglas.marq.alves@outlook.com>
2018-10-17 22:22:15 -0300

    Rearrajando algumas pastas

--- src/Model/Secretaria.java
+++ src/Model/Secretaria.java
@@ -5,7 +5,7 @@
  */
 package model;
 
-import model.dao.PacienteDao;
+import dao.PacienteDao;
 
 /**
  *
--- src/dao/PacienteDao.java
+++ src/dao/PacienteDao.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model.dao;
+package dao;
 
+import dao.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
