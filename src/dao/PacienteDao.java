af7ed85b7349ee8885b09b6f6180f6e488455a15
Gabriel Martins <34401129+gabrielmart@users.noreply.github.com>
2018-10-18 03:00:25 -0300

    Refatoração do DAO e Model
    Foi refatorado os metodos da classe PacienteDao e  adicionado novos campos em algumas classes do model

--- src/dao/PacienteDao.java
+++ src/dao/PacienteDao.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import model.Endereco;
 import model.EstadoCivil;
@@ -22,95 +23,197 @@ import model.Sexo;
  */
 public class PacienteDao implements dao<Paciente> {
 
-	@Override
-	public void create(Paciente obj) {
-		Connection con = ConnectionFactory.getConnection();
-		PreparedStatement pst = null;
+    @Override
+    public void create(Paciente obj) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement pst = null;
 
-		try {
-			pst = con.prepareStatement("insert into Pacientes (id, nome, rg, dataNascimento, logradouro , estadoCivil, sexo, convenio)"
-				+ "values (?,?,?,?,?,?,?,?)");
+        try {
+            pst = con.prepareStatement("insert into d1_Pacientes (cd_Paciente, nm_paciente, ds_cpf, dt_Nascimento, ds_rg, ds_Telefone, ds_Celular, ds_Email,"
+                    + " ds_Convenio, ds_logradouro, ds_numeroresidencia, ds_complemento, ds_bairro, ds_cidade, cd_cep, ds_sexo,"
+                    + " ds_estadocivil)"
+                    + " values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
-			pst.setLong(1, obj.getId());
-			pst.setString(2, obj.getNome());
-			pst.setString(3, obj.getRg());
-			pst.setString(4, obj.getDataNascimento());
-			pst.setString(5, obj.getEndereco().toString());
-			pst.setString(6, obj.getEstadoCivil().toString());
-			pst.setString(7, obj.getSexo().toString());
-			pst.setString(8, obj.getConvenio());
+            pst.setLong(1, obj.getId());
+            pst.setString(2, obj.getNome());
+            pst.setString(3, obj.getCpf());
+            pst.setString(4, obj.getDataNascimento());
+            pst.setString(5, obj.getRg());
+            pst.setString(6, obj.getTelefone());
+            pst.setString(7, obj.getCelular());
+            pst.setString(8, obj.getEmail());
+            pst.setString(9, obj.getConvenio());
+            pst.setString(10, obj.getEndereco().getLogradouro());
+            pst.setString(11, obj.getEndereco().getNumero());
+            pst.setString(12, obj.getEndereco().getComplemento());
+            pst.setString(13, obj.getEndereco().getBairro());
+            pst.setString(14, obj.getEndereco().getCidade());
+            pst.setLong(15, obj.getEndereco().getCep());
+            pst.setString(16, obj.getSexo().toString());
+            pst.setString(17, obj.getEstadoCivil().toString());
 
-		} catch (SQLException ex) {
-			throw new RuntimeException("Erro no Cadastro do Paciente");
-		} finally {
-			ConnectionFactory.closeConnection(con);
-		}
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro no Cadastro do Paciente");
+        } finally {
+            ConnectionFactory.closeConnection(con);
+        }
 
-	}
+    }
 
-	@Override
-	public List<Paciente> read() {
-		Connection con = ConnectionFactory.getConnection();
-		PreparedStatement pst = null;
-		ResultSet rs = null;
-		
-		try {
-			pst = con.prepareCall("select * from Pacientes");
-			rs = pst.executeQuery();
+    @Override
+    public List<Paciente> read() {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement pst = null;
+        ResultSet rs = null;
 
-			while (rs.next()) {
+        try {
+            pst = con.prepareStatement("select * from d1_Pacientes");
+            rs = pst.executeQuery();
+            List pacientes = new ArrayList();
 
-				long id = rs.getLong("Id");
-				String nome = rs.getString("Nome");
-				String rg = rs.getString("Rg");
-				String dataNascimento = rs.getString("DataNascimento");
-				String estadoCivil = rs.getString("EstadoCivil");
-				String sexo = rs.getString("Sexo");
-				String convenio = rs.getString("Convenio");
+            while (rs.next()) {
+                Long id = rs.getLong("cd_paciente");
+                String nome = rs.getString("nm_paciente");
+                String cpf = rs.getString("ds_cpf");
+                String dataNascimento = rs.getString("dt_nascimento");
+                String rg = rs.getString("ds_rg");
+                String telefone = rs.getString("ds_Telefone");
+                String celular = rs.getString("ds_Celular");
+                String email = rs.getString("ds_Email");
+                String convenio = rs.getString("ds_Convenio");
+                String lougradouro = rs.getString("ds_lougradouro");
+                String numeroEndereco = rs.getString("ds_numeroresidencia");
+                String complemento = rs.getString("ds_complemento");
+                String bairro = rs.getString("ds_bairro");
+                String cidade = rs.getString("ds_cidade");
+                long cep = rs.getLong("cd_cep");
+                String sexo = rs.getString("ds_sexo");
+                String estadoCivil = rs.getString("ds_estadocivil");
 
-				//String logradouro = //GABRIEL EDITA AQUI DPS
-					
-					
-				//new Endereco(logradouro, numero, complemento, bairro, cidade, cep);
-				//new Paciente(convenio, id, nome, rg, dataNascimento, endereco, EstadoCivil.valueOf(estadoCivil), Sexo.valueOf(sexo));
-			}
+                Endereco endereco = new Endereco(lougradouro, numeroEndereco, complemento, bairro, cidade, cep);
+                Paciente paciente = new Paciente(convenio, id, nome, cpf, rg, dataNascimento, telefone, celular,
+                        email, endereco, EstadoCivil.valueOf(estadoCivil), Sexo.valueOf(sexo));
+                pacientes.add(paciente);
 
-		} catch (SQLException ex) {
-			throw new RuntimeException("Erro na Leitura da Tabela Paciente");
-		} finally {
-			ConnectionFactory.closeConnection(con);
-		}
-            return null;
-	}
+            }
 
-	@Override
-	public void update(Paciente obj) {
-		Connection con = ConnectionFactory.getConnection();
-		PreparedStatement pst;
-		
-			
-			
-	}
+            return pacientes;
 
-	@Override
-	public void delete(Paciente obj) {
-		Connection con = ConnectionFactory.getConnection();
-		PreparedStatement pst;
-		
-		try {
-			pst = con.prepareStatement("delete from Paciente"
-						+ "where id = ?");
-			pst.setLong(1, obj.getId());
-		} catch (SQLException ex) {
-			throw new RuntimeException("Erro ao Deletar Paciente!");
-		} finally {
-			ConnectionFactory.closeConnection(con);
-		}
-	}
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro na Leitura da Tabela Paciente");
+        } finally {
+            ConnectionFactory.closeConnection(con);
+        }
 
-	@Override
-	public Paciente findById(long id) {
-		return null;
-	}
+    }
+
+    @Override
+    public void update(Paciente obj) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement pst;
+
+        try {
+            pst = con.prepareStatement("update d1_Paciente"
+                    + " set cd_Paciente = ?"
+                    + "nm_Paciente = ?,"
+                    + "ds_cpf = ?,"
+                    + "dt_Nascimento = ?,"
+                    + "ds_rg = ?,"
+                    + "ds_Telefone = ?,"
+                    + "ds_Celular = ?,"
+                    + "ds_Email = ?,"
+                    + "ds_Convenio = ?,"
+                    + "ds_logradouro = ?,"
+                    + "ds_numeroresidencia = ?,"
+                    + "ds_complemento = ?,"
+                    + "ds_bairro = ?,"
+                    + "ds_cidade = ?,"
+                    + "cd_cep = ?,"
+                    + "ds_sexo = ?,"
+                    + "ds_estadocivil = ?");
+
+            pst.setLong(1, obj.getId());
+            pst.setString(2, obj.getNome());
+            pst.setString(3, obj.getCpf());
+            pst.setString(4, obj.getDataNascimento());
+            pst.setString(5, obj.getRg());
+            pst.setString(6, obj.getTelefone());
+            pst.setString(7, obj.getCelular());
+            pst.setString(8, obj.getEmail());
+            pst.setString(9, obj.getConvenio());
+            pst.setString(10, obj.getEndereco().getLogradouro());
+            pst.setString(11, obj.getEndereco().getNumero());
+            pst.setString(12, obj.getEndereco().getComplemento());
+            pst.setString(13, obj.getEndereco().getBairro());
+            pst.setString(14, obj.getEndereco().getCidade());
+            pst.setLong(15, obj.getEndereco().getCep());
+            pst.setString(16, obj.getSexo().toString());
+            pst.setString(17, obj.getEstadoCivil().toString());
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro na atualização dos dados do paciente");
+        } finally {
+            ConnectionFactory.closeConnection(con);
+        }
+
+    }
+
+    @Override
+    public void delete(Paciente obj) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement pst;
+
+        try {
+            pst = con.prepareStatement("delete from Paciente"
+                    + "where id = ?");
+            pst.setLong(1, obj.getId());
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro ao Deletar Paciente!");
+        } finally {
+            ConnectionFactory.closeConnection(con);
+        }
+    }
+
+    @Override
+    public Paciente findById(long idFind) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        try {
+            pst = con.prepareStatement("select * from d1_Pacientes where cd_Paciente = ?");
+            pst.setLong(1, idFind);
+            rs = pst.executeQuery();
+            
+            Long id = rs.getLong("cd_paciente");
+            String nome = rs.getString("nm_paciente");
+            String cpf = rs.getString("ds_cpf");
+            String dataNascimento = rs.getString("dt_nascimento");
+            String rg = rs.getString("ds_rg");
+            String telefone = rs.getString("ds_Telefone");
+            String celular = rs.getString("ds_Celular");
+            String email = rs.getString("ds_Email");
+            String convenio = rs.getString("ds_Convenio");
+            String lougradouro = rs.getString("ds_lougradouro");
+            String numeroEndereco = rs.getString("ds_numeroresidencia");
+            String complemento = rs.getString("ds_complemento");
+            String bairro = rs.getString("ds_bairro");
+            String cidade = rs.getString("ds_cidade");
+            long cep = rs.getLong("cd_cep");
+            String sexo = rs.getString("ds_sexo");
+            String estadoCivil = rs.getString("ds_estadocivil");
+
+            Endereco endereco = new Endereco(lougradouro, numeroEndereco, complemento, bairro, cidade, cep);
+            Paciente paciente = new Paciente(convenio, id, nome, cpf, rg, dataNascimento, telefone, celular,
+                    email, endereco, EstadoCivil.valueOf(estadoCivil), Sexo.valueOf(sexo));
+
+            return paciente;
+
+        } catch (SQLException ex) {
+            throw new RuntimeException("Erro na Leitura da Tabela Paciente");
+        } finally {
+            ConnectionFactory.closeConnection(con);
+        }
+
+    }
 
 }
